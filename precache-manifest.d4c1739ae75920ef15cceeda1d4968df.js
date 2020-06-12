8768d56c0009e42c19af1852124873b62ca11dda
amazp <muktiarafi@gmail.com>
2020-06-12 20:02:31 +0700

    Updates

---
+++ precache-manifest.d4c1739ae75920ef15cceeda1d4968df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3513d1289c68df238b3eaee1ea3333e4",
+    "url": "/simple-perpustakaan-crud/index.html"
+  },
+  {
+    "revision": "905b6e7768ec0047bec3",
+    "url": "/simple-perpustakaan-crud/static/css/main.a98249bf.chunk.css"
+  },
+  {
+    "revision": "d13a9c33805ab53f5c55",
+    "url": "/simple-perpustakaan-crud/static/js/2.d821d8eb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-perpustakaan-crud/static/js/2.d821d8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "905b6e7768ec0047bec3",
+    "url": "/simple-perpustakaan-crud/static/js/main.78b488f2.chunk.js"
+  },
+  {
+    "revision": "9f303bdb1c70e40a28c0",
+    "url": "/simple-perpustakaan-crud/static/js/runtime-main.d22e0f64.js"
+  }
+]);
